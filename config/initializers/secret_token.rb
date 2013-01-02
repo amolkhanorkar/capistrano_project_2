2c48b2b8968bce3e65ad20ba796797ce16a2daef
Amol <webonise@amol-desktop.(none)>
2013-01-02 15:03:13 +0530

    add sample app files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '0cbe1d4b04ae1d98602f290e4c35e8cc921dc59ff4106bc80858402ebc410dee94a6c26be981c54e08262e13b845a3420f887d4ccf6b0c50692cfabda0f25418'
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Amol K"
+    email    "ak@gmail.com"
+    password "amolkkk"
+    password_confirmation "amolkkk"
+  end
+end 
