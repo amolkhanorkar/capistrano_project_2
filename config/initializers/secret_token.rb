b2e48fb2a1098d6ae3755a0434bd267ffd8a3ce7
Amol <webonise@amol-desktop.(none)>
2013-01-02 14:59:03 +0530

    remove entries

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-DemoApp::Application.config.secret_token = 'd5839cdf014bce02b2f26fa16d15438382d9b452c5bf9d73bf32265d101497e387e5a310e9d91797f32f392c0803b91f1d972b93b41de1298b56a158f9d2c150'
